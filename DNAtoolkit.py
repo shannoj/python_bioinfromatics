bf2003a6111fc6f0fdea49945ba489a5bf673fdd
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-29 14:57:33 +0100

    added GC content counters functions

--- DNAtoolkit.py
+++ DNAtoolkit.py
@@ -17,3 +17,13 @@ def transcription(seq):
 
 def reverse_complement(seq):
     return ''.join([DNA_Revers_Complement[nuc] for nuc in seq])[::-1]
+
+def gc_content(seq):
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_subsec(seq, k=20):
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i:i + k]
+        res.append(gc_content(subseq))
+    return res
