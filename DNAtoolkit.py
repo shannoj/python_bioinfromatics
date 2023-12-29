d6422f8222093f07a7c191e7e27234916768f193
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-29 14:39:47 +0100

    added transcription and reverse complement functions

--- DNAtoolkit.py
+++ DNAtoolkit.py
@@ -2,6 +2,8 @@ import collections
 
 Nucleotides = ["A", "T", "C", "G"]
 
+DNA_Revers_Complement = {"A": "T", "T": "A", "C": "G", "G": "C"}
+
 def validateSeq(dna_seq):
     tmpseq = dna_seq.upper()
     for nuc in tmpseq:
@@ -11,3 +13,10 @@ def validateSeq(dna_seq):
 
 def countNucFrequency(seq):
     return dict(collections.Counter(seq))
+
+def transcription(seq):
+    """DNA -> RNA Transcription. Replacing Thymine with Uracil"""
+    return seq.replace("T", "U")
+
+def reverse_complement(seq):
+    return ''.join([DNA_Revers_Complement[nuc] for nuc in seq])[::-1]
